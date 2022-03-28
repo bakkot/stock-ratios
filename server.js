f06b6b81262864f5cb180581c1ca5661edc57fce
Kevin Gibbons <bakkot@gmail.com>
2022-03-27 21:01:58 -0700

    testing

--- server.js
+++ server.js
@@ -6,13 +6,18 @@ fastify.get('/', async (request, reply) => {
   return { hello: 'world' };
 });
 
-// Run the server!
-const start = async () => {
-  try {
-    await fastify.listen(3000);
-  } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-};
-start();
+export default async (req, res) => {
+    await fastify.ready();
+    fastify.server.emit('request', req, res);
+}
+
+// // Run the server!
+// const start = async () => {
+//   try {
+//     await fastify.listen(3000);
+//   } catch (err) {
+//     fastify.log.error(err);
+//     process.exit(1);
+//   }
+// };
+// start();
